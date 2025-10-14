63fb53dcaafb8b31cb2bb37f7a8e7f41b8111e16
PhucQuangg <nguyenhoangphuc020803@gmail.com>
2025-10-14 19:20:50 +0700

    view room for student

---
+++ src/main/java/com/stu/edu/ktx_management/controller/StudentRoomController.java
@@ -0,0 +1,30 @@
+package com.stu.edu.ktx_management.controller;
+
+import com.stu.edu.ktx_management.entity.Room;
+import com.stu.edu.ktx_management.entity.TypeRoom;
+import com.stu.edu.ktx_management.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student/rooms")
+public class StudentRoomController {
+
+    @Autowired
+    private RoomService roomService;
+
+    @GetMapping("/available-by-type")
+    public List<Room> getRoomsByType(@RequestParam("type") String type) {
+        TypeRoom roomType;
+        try {
+            roomType = TypeRoom.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Loại phòng không hợp lệ: " + type);
+        }
+
+        return roomService.getAvailableRoomsByType(roomType);
+    }
+}
