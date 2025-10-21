d1b32229dcf7995195df34792feff89ced56268a
PhucQuangg <nguyenhoangphuc020803@gmail.com>
2025-10-21 15:50:11 +0700

    Restructure project: add frontend-admin, frontend-student, and Docker setup

---
+++ frontend_admin/src/main/java/com/stu/edu/frontend_admin/FrontendAdminApplication.java
@@ -0,0 +1,13 @@
+package com.stu.edu.frontend_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendAdminApplication.class, args);
+	}
+
+}
---
+++ frontend_student/src/test/java/com/stu/edu/frontend_student/FrontendStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.edu.frontend_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
